b90f8a3fb6601bfcdc6de565d6fb73e88f2615e6
tytus42 <103448484+tytus42@users.noreply.github.com>
2025-06-30 15:21:19 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -2,19 +2,19 @@ const allCards = [
     // === KARTY NEUTRALNE ===
     { id: 'ciri_1', name: 'Ciri', type: 'Unit', power: 15, isHero: true, faction: 'Neutral', row: 'melee' },
     { id: 'geralt_of_rivia_1', name: 'Geralt z Rivii', type: 'Unit', power: 15, isHero: true, faction: 'Neutral', row: 'melee' },
-    { id: 'dandelion_1', name: 'Jaskier', type: 'Unit', power: 2, isHero: false, faction: 'Neutral', row: 'melee', ability: 'morale_boost' },
-    { id: 'dandelion_2', name: 'Jaskier', type: 'Unit', power: 2, isHero: false, faction: 'Neutral', row: 'melee', ability: 'morale_boost' },
-    { id: 'zoltan_chivay_1', name: 'Zoltan Chivay', type: 'Unit', power: 5, isHero: false, faction: 'Neutral', row: 'melee', ability: 'scorch_row' },
-    { id: 'avallach_1', name: 'Avallac\'h', type: 'Unit', power: 0, isHero: false, faction: 'Neutral', row: 'melee', ability: 'spy' },
-    { id: 'vesemir_1', name: 'Vesemir', type: 'Unit', power: 6, isHero: false, faction: 'Neutral', row: 'melee' },
+    { id: 'dandelion_1', name: 'Jaskier', type: 'Unit', power: 2, isHero: false, faction: 'Neutral', row: 'melee', abilities: 'morale_boost' },
+    { id: 'dandelion_2', name: 'Jaskier', type: 'Unit', power: 2, isHero: false, faction: 'Neutral', row: 'melee', abilities: 'morale_boost' },
+    { id: 'zoltan_chivay_1', name: 'Zoltan Chivay', type: 'Unit', power: 5, isHero: false, faction: 'Neutral', row: 'melee', abilities: 'scorch_row' },
+    { id: 'avallach_1', name: 'Avallac\'h', type: 'Unit', power: 0, isHero: false, faction: 'Neutral', row: 'melee', abilities: 'spy' },
+    { id: 'vesemir_1', name: 'Vesemir', type: 'Unit', power: 6, isHero: false, faction: 'Neutral', row: ['melee', 'ranged'], abilities: ['agile', 'morale_boost'] },
     
     // === KARTY SPECJALNE (NEUTRALNE) ===
-    { id: 'scorch_fire_rain_1', name: 'Deszcz Ognia', type: 'Special', faction: 'Neutral', ability: 'scorch_strongest' },
-    { id: 'scorch_fire_rain_2', name: 'Deszcz Ognia', type: 'Special', faction: 'Neutral', ability: 'scorch_strongest' },
-    { id: 'commanders_horn_1', name: 'Róg Dowódcy', type: 'Special', faction: 'Neutral', ability: 'horn' },
-    { id: 'commander_horn_2', name: 'Róg Dowódcy', type: 'Special', faction: 'Neutral', ability: 'horn' },
-    { id: 'decoy_1', name: 'Wabik', type: 'Special', faction: 'Neutral', ability: 'decoy' },
-    { id: 'decoy_2', name: 'Wabik', type: 'Special', faction: 'Neutral', ability: 'decoy' },
+    { id: 'scorch_fire_rain_1', name: 'Deszcz Ognia', type: 'Special', faction: 'Neutral', abilities: 'scorch_strongest' },
+    { id: 'scorch_fire_rain_2', name: 'Deszcz Ognia', type: 'Special', faction: 'Neutral', abilities: 'scorch_strongest' },
+    { id: 'commanders_horn_1', name: 'Róg Dowódcy', type: 'Special', faction: 'Neutral', abilities: 'horn' },
+    { id: 'commander_horn_2', name: 'Róg Dowódcy', type: 'Special', faction: 'Neutral', abilities: 'horn' },
+    { id: 'decoy_1', name: 'Wabik', type: 'Special', faction: 'Neutral', abilities: 'decoy' },
+    { id: 'decoy_2', name: 'Wabik', type: 'Special', faction: 'Neutral', abilities: 'decoy' },
     
     // === KARTY POGODY (NEUTRALNE) ===
     { id: 'biting_frost_1', name: 'Trzaskający Mróz', type: 'Weather', faction: 'Neutral', rowAffected: ['melee'] },
@@ -27,13 +27,13 @@ const allCards = [
     { id: 'foltest_1', name: 'Foltest: Król Temerii', type: 'Leader', faction: 'Northern Realms' },
     { id: 'yennefer_of_vengerberg_1', name: 'Yennefer z Vengerbergu', type: 'Unit', power: 7, isHero: true, faction: 'Northern Realms', row: 'ranged' },
     { id: 'triss_merigold_1', name: 'Triss Merigold', type: 'Unit', power: 7, isHero: true, faction: 'Northern Realms', row: 'ranged' },
-    { id: 'blue_stripes_commando_1', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', ability: 'tight_bond', baseId: 'blue_stripes_commando' },
-    { id: 'blue_stripes_commando_2', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', ability: 'tight_bond', baseId: 'blue_stripes_commando' },
-    { id: 'blue_stripes_commando_3', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', ability: 'tight_bond', baseId: 'blue_stripes_commando' },
-    { id: 'catapult_1', name: 'Katapulta', type: 'Unit', power: 8, faction: 'Northern Realms', row: 'siege', ability: 'tight_bond', baseId: 'catapult' },
-    { id: 'catapult_2', name: 'Katapulta', type: 'Unit', power: 8, faction: 'Northern Realms', row: 'siege', ability: 'tight_bond', baseId: 'catapult' },
-    { id: 'field_medic_1', name: 'Felczer', type: 'Unit', power: 5, isHero: false, faction: 'Northern Realms', row: 'ranged', ability: 'medic' },
-    { id: 'field_medic_2', name: 'Felczer', type: 'Unit', power: 5, isHero: false, faction: 'Northern Realms', row: 'ranged', ability: 'medic' },
+    { id: 'blue_stripes_commando_1', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', abilities: 'tight_bond', baseId: 'blue_stripes_commando' },
+    { id: 'blue_stripes_commando_2', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', abilities: 'tight_bond', baseId: 'blue_stripes_commando' },
+    { id: 'blue_stripes_commando_3', name: 'Komandos Niebieskich Pasów', type: 'Unit', power: 4, faction: 'Northern Realms', row: 'melee', abilities: 'tight_bond', baseId: 'blue_stripes_commando' },
+    { id: 'catapult_1', name: 'Katapulta', type: 'Unit', power: 8, faction: 'Northern Realms', row: 'siege', abilities: 'tight_bond', baseId: 'catapult' },
+    { id: 'catapult_2', name: 'Katapulta', type: 'Unit', power: 8, faction: 'Northern Realms', row: 'siege', abilities: 'tight_bond', baseId: 'catapult' },
+    { id: 'field_medic_1', name: 'Felczer', type: 'Unit', power: 5, isHero: false, faction: 'Northern Realms', row: 'ranged', abilities: 'medic' },
+    { id: 'field_medic_2', name: 'Felczer', type: 'Unit', power: 5, isHero: false, faction: 'Northern Realms', row: 'ranged', abilities: 'medic' },
     
       { id: 'eredin_1', name: 'Eredin', type: 'Leader', faction: 'Monsters', abilityDesc: 'Wybierz kartę Pogody z talii i zagraj ją natychmiast.' },
 
@@ -42,11 +42,11 @@ const allCards = [
     // ============================================
     // --- Wręcz (Melee) ---
     { id: 'imlerith_1', name: 'Imlerith', type: 'Unit', power: 10, isHero: true, faction: 'Monsters', row: 'melee' },
-    { id: 'nekker_1', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', ability: 'muster' },
-    { id: 'nekker_2', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', ability: 'muster' },
-    { id: 'nekker_3', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', ability: 'muster' },
-    { id: 'ghoul_1', name: 'Ghul', type: 'Unit', power: 1, isHero: false, faction: 'Monsters', row: 'melee', ability: 'muster' },
-    { id: 'ghoul_2', name: 'Ghul', type: 'Unit', power: 1, isHero: false, faction: 'Monsters', row: 'melee', ability: 'muster' },
+    { id: 'nekker_1', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', abilities: 'muster' },
+    { id: 'nekker_2', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', abilities: 'muster' },
+    { id: 'nekker_3', name: 'Nekker', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'melee', abilities: 'muster' },
+    { id: 'ghoul_1', name: 'Ghul', type: 'Unit', power: 1, isHero: false, faction: 'Monsters', row: 'melee', abilities: 'muster' },
+    { id: 'ghoul_2', name: 'Ghul', type: 'Unit', power: 1, isHero: false, faction: 'Monsters', row: 'melee', abilities: 'muster' },
     // --- Dystans (Ranged) ---
     { id: 'wyvern_1', name: 'Wiwerna', type: 'Unit', power: 5, isHero: false, faction: 'Monsters', row: 'ranged' },
     { id: 'harpy_1', name: 'Harpia', type: 'Unit', power: 2, isHero: false, faction: 'Monsters', row: 'ranged' },
